7b65bb10cf5d612636375203e0e617b486945967
scveloso <samtveloso@gmail.com>
2018-09-30 21:46:28 -0700

    Sample commit to non-master branch

--- app/src/main/java/com/ubclaunchpad/room8/MainActivity.java
+++ app/src/main/java/com/ubclaunchpad/room8/MainActivity.java
@@ -2,12 +2,17 @@ package com.ubclaunchpad.room8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSample = findViewById(R.id.btnSample);
     }
 }
