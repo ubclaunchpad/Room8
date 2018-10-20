624fdf3c027fe604ac254f9c90a27c960a986044
Yutian Lin <me@yutianlin.com>
2018-10-20 11:29:22 -0700

    some buttons added to login

--- app/src/main/java/com/ubclaunchpad/room8/LoginActivity.java
+++ app/src/main/java/com/ubclaunchpad/room8/LoginActivity.java
@@ -2,12 +2,23 @@ package com.ubclaunchpad.room8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+                break;
+            case R.id.forgot_password:
+                break;
+        }
     }
 }
