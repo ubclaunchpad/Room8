c88fb32327110aea0c4578af59a0b0d20cbdad90
scveloso <samtveloso@gmail.com>
2018-10-28 23:21:02 -0700

    Cleaned up LoginActivity and SignUpActivity to depict what is illustrated in the wireframe and to help design later.

--- app/src/main/java/com/ubclaunchpad/room8/SignUpActivity.java
+++ app/src/main/java/com/ubclaunchpad/room8/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.ubclaunchpad.room8;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.PatternMatcher;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,7 +24,6 @@ import com.google.firebase.database.FirebaseDatabase;
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText mEmail, mPassword, mFirstName, mLastName;
-
     private FirebaseAuth mAuth;
 
     @Override
@@ -30,16 +31,19 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        mEmail = (EditText) findViewById(R.id.sign_up_et_email);
-        mPassword = (EditText) findViewById(R.id.sign_up_et_password);
+        mEmail = findViewById(R.id.sign_up_et_email);
+        mPassword = findViewById(R.id.sign_up_et_password);
         mFirstName = findViewById(R.id.sign_up_et_firstname);
         mLastName = findViewById(R.id.sign_up_et_lastname);
 
         mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.sign_up_btn_sign_up).setOnClickListener(this);
-        findViewById(R.id.sign_up_btn_login).setOnClickListener(this);
+        TextView txtSignUp = findViewById(R.id.txtSignUp);
+        txtSignUp.setOnClickListener(this);
+        txtSignUp.setPaintFlags(txtSignUp.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
 
+        findViewById(R.id.sign_up_btn_sign_up).setOnClickListener(this);
+        findViewById(R.id.txtSignIn).setOnClickListener(this);
     }
 
 
@@ -99,10 +103,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.sign_up_btn_sign_up:
                 registerUser();
                 break;
-            case R.id.sign_up_btn_login:
+            case R.id.txtSignIn:
                 startActivity(new Intent(this, LoginActivity.class));
-                break;
-            case R.id.sign_up_btn_google:
                 break;
         }
     }
