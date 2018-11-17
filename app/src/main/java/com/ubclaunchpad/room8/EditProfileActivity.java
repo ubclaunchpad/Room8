e46e5490932be23388e724473771932327f13ec8
hyunuklim <hyunuk@alumni.ubc.ca>
2018-11-17 12:27:32 -0800

    Implemented display email, first name, and last name.
    Will implement if button clicks, store modified first and last name
     and change password.

--- app/src/main/java/com/ubclaunchpad/room8/EditProfileActivity.java
+++ app/src/main/java/com/ubclaunchpad/room8/EditProfileActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,41 +15,37 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class EditProfileActivity extends AppCompatActivity implements View.OnClickListener {
-    private DatabaseReference databaseReference;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private DatabaseReference userRef;
+
     private FirebaseAuth mAuth;
+    private String currUserUID;
     private FirebaseUser currUser;
     private String firstName;
     private String lastName;
     private String email;
+    private TextView show_email;
+    private TextView edit_first_name;
+    private TextView edit_last_name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        userRef = myRef.child("Users");
         mAuth = FirebaseAuth.getInstance();
-        databaseReference = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference userRef = databaseReference.child("Users").child(mAuth.getCurrentUser().getUid());
-        ValueEventListener eventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    if (ds.getKey().toString().equals(mAuth.getCurrentUser().getUid())) {
+        currUserUID = mAuth.getCurrentUser().getUid();
 
-                        String afirstName = ds.child("FirstName").getValue(String.class);
-                        String alastName = ds.child("LastName").getValue(String.class);
-                        String aemail = ds.child("Email").getValue(String.class);
-                        System.out.println(afirstName);
-                        System.out.println(alastName);
-                        System.out.println(aemail);
-                    }
+        show_email = (TextView) findViewById(R.id.show_email);
+        edit_first_name = (TextView) findViewById(R.id.edit_first_name);
+        edit_last_name = (TextView) findViewById(R.id.edit_last_name);
+        displayUserInfo();
 
-                    //should store data -> settext on the layout
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {}};
-        userRef.addListenerForSingleValueEvent(eventListener);
 
     }
 
@@ -56,7 +53,7 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
         return new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     if (ds.getKey().toString().equals(mAuth.getCurrentUser().getUid())) {
 
                         String afirstName = ds.child("FirstName").getValue(String.class);
@@ -72,12 +69,40 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {}
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
         };
     }
 
     @Override
     public void onClick(View view) {
+
+    }
+
+    private void displayUserInfo() {
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+                    if (user.Uid.equals(currUserUID)) {
+                        email = user.Email;
+                        firstName = user.FirstName;
+                        lastName = user.LastName;
+                    }
+                }
+                show_email.setText(email);
+                edit_first_name.setText(firstName);
+                edit_last_name.setText(lastName);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
