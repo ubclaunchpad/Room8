5b81d688aba01e21e6b6722757842344b1ae18cd
scveloso <samtveloso@gmail.com>
2019-02-10 14:42:10 -0800

    Clean up variable names, add documentation to Activities, complete styling on newer Activities

--- app/src/main/java/com/ubclaunchpad/room8/exception/PasswordsNotMatch.java
+++ app/src/main/java/com/ubclaunchpad/room8/exception/PasswordsNotMatch.java
@@ -1,11 +1,11 @@
 package com.ubclaunchpad.room8.exception;
 
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class PasswordsNotMatch extends Throwable {
 
-    public PasswordsNotMatch(String errorMessage, TextView textView) {
-        textView.setError(errorMessage);
-        textView.requestFocus();
+    public PasswordsNotMatch(String errorMessage, EditText editText) {
+        editText.setError(errorMessage);
+        editText.requestFocus();
     }
 }
