11d7d9561699ced527680e12097a86b0a7a5c707
Dan_Y <daniel.yd.liu@gmail.com>
2019-02-22 01:08:15 -0800

    Implement view holder creation

--- app/src/main/java/com/ubclaunchpad/room8/adapter/HouseRulesAdapter.java
+++ app/src/main/java/com/ubclaunchpad/room8/adapter/HouseRulesAdapter.java
@@ -1,7 +1,9 @@
 package com.ubclaunchpad.room8.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -42,7 +44,14 @@ public class HouseRulesAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View houseRuleView = inflater.inflate(R.layout.item_house_rule, parent, false);
+
+        // Return a new holder instance
+        return new ViewHolder(houseRuleView);
     }
 
     @Override
