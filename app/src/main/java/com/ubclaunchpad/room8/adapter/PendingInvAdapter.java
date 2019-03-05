09f00a721f2171aa9f45f985e008aa6c28797e0c
Dan_Y <daniel.yd.liu@gmail.com>
2019-03-05 15:37:06 -0800

    Bugfix in pending inv adapter

--- app/src/main/java/com/ubclaunchpad/room8/adapter/PendingInvAdapter.java
+++ app/src/main/java/com/ubclaunchpad/room8/adapter/PendingInvAdapter.java
@@ -93,7 +93,7 @@ public class PendingInvAdapter extends
             @Override
             public void onClick(View v) {
                 acceptInvite(groupName);
-                Intent groupIntent = GroupActivity.createIntent(v.getContext(), mCurrUserUid, groupName);
+                Intent groupIntent = GroupActivity.createIntent(v.getContext(), groupName, mCurrUserUid);
                 v.getContext().startActivity(groupIntent);
             }
         });
