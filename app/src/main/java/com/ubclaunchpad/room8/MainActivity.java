14a03886dc4c75bd2e85d5e61751afd3556d4532
Dan_Y <daniel.yd.liu@gmail.com>
2019-06-04 18:58:23 -0700

    Updated Main Activity UI

--- app/src/main/java/com/ubclaunchpad/room8/MainActivity.java
+++ app/src/main/java/com/ubclaunchpad/room8/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.common.SignInButton;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignInButton googleSignInButton = findViewById(R.id.sign_in_button);
+        googleSignInButton.setSize(SignInButton.SIZE_WIDE);
+        googleSignInButton.setOnClickListener(this);
 
         findViewById(R.id.btnSignUp).setOnClickListener(this);
         findViewById(R.id.btnLogin).setOnClickListener(this);
